93070a62b47dd0c64017a1466e057c3ba80b4001
santhoshkumarasamy <santhoshkumarkumarasamy850@gmail.com>
2022-03-08 08:57:08 +0530

    COmplted : day 5

--- Server_basics/app.js
+++ Server_basics/app.js
@@ -4,27 +4,36 @@ const express = require('express');
 
 const app = express();
 
+
+app.set('view engine','ejs') // this set the view engine to ejs
+
 // setting it to listen in port 3000
 
 app.listen(3000);
 
 //setting routing
 app.get('/',(req,res)=>{
-    // res.send('<p>Hi There</p>')
-    res.sendFile('./htmlFiles/index.html',{root: __dirname}) //sendFile: function will take files from absolute path(meaning i.e it will take from computer folder path) so we have mention starting directory location where the files are
+
+    // if the file we send is created from a view engine we have to use render function to send details
+    res.render('index',{title:"Home"}) // we can pass values for a ejs file by sending an object in the render function, the values can be directly accessed by the key alone
 })
 app.get('/about',(req,res)=>{
-    res.sendFile('./htmlFiles/about.html',{root: __dirname})
+   res.render('about',{title:"About"})
 })
 
+
+
+app.get('/blogs/create',(req,res)=>{
+    res.render('create',{title:"Create"})
+})
 //redirects
 
-app.get('/about-us',(req,res)=>{
-    res.redirect('/about')
-})
+// app.get('/about-us',(req,res)=>{
+//     res.redirect('/about')
+// })
 
 // 404
 
 app.use((req,res)=>{ //this method will be fired whenever the execution reaches hear, when request is made express will check for the url requested, if non of the above url are matched before this function, this response will we triggered. When a response is triggered express doesn't execute the code below the response function so th 404 (use function) should be at the bottom
-    res.status(404).sendFile('./htmlFiles/404.html',{root:__dirname})
+    res.status(404).render('404',{title:"404"})
 })
--- Server_basics/app1.js
+++ Server_basics/app1.js
@@ -6,7 +6,7 @@ const app = express();
 
 // setting up the view engine
 
-app.set('view engine','ejs') // this set the view engine to ejs
+
 
 // app.set('views','htmlFiles') // by default the express will look for view templates in views folder to set to an some other folder this code is used.
 
